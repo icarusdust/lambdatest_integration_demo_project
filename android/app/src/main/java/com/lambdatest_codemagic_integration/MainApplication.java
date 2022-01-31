14504538b12dd6a3520ec29a26a16b7b1d0349d9
Nihal Aghazada <nihal@nevercode.io>
2022-01-31 14:43:49 +0100

    testing

--- android/app/src/main/java/com/lambdatest_codemagic_integration/MainApplication.java
+++ android/app/src/main/java/com/lambdatest_codemagic_integration/MainApplication.java
@@ -80,61 +80,112 @@ package com.lambdatest_codemagic_integration;
 // }
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+/**
+ * An {@link Activity} that gets a text string from the user and displays it back when the user
+ * clicks on one of the two buttons. The first one shows it in the same activity and the second
+ * one opens another activity and displays the message.
+ */
+public class MainActivity extends Activity implements View.OnClickListener {
 
+    // The TextView used to display the message inside the Activity.
+    private TextView mTextView;
 
+    // The EditText where the user types the message.
+    private EditText mEditText;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-import java.net.URL;
-import java.util.List;
-import java.net.MalformedURLException;
-import io.appium.java_client.MobileBy;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.remote.DesiredCapabilities;
-public class AndroidApp {
-public static String userName = "nihalnevercode";
-public static String accessKey = "tZAy17ivs9JlLPDqjB3n3sZxZq3K1SzPmaGwOFNddEG9kdYeJt";
-public static void main(String args[]) throws MalformedURLException, InterruptedException {
-DesiredCapabilities capabilities = new DesiredCapabilities();
-capabilities.setCapability("platformName", "Android");
-capabilities.setCapability("deviceName", "Google Pixel 3");
-capabilities.setCapability("isRealMobile", true);
-capabilities.setCapability("platformVersion","10");
-capabilities.setCapability("app","lt://APP100201711643634891400553");
-capabilities.setCapability("deviceOrientation", "PORTRAIT");
-capabilities.setCapability("console",true);
-capabilities.setCapability("network",true);
-capabilities.setCapability("visual",true);
-RemoteWebDriver driver = new RemoteWebDriver(new
-URL("https://"+userName+":"+accessKey+"@beta-hub.lambdatest.com/wd/hub"), capabilities);
-try {
-AndroidElement searchElement = (AndroidElement) new WebDriverWait(driver, 30).until(
-ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Search
-Wikipedia")));
-searchElement.click();
-AndroidElement insertTextElement = (AndroidElement) new WebDriverWait(driver, 30).until(
-ExpectedConditions.elementToBeClickable(MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
-insertTextElement.sendKeys("LambdaTest"+ Keys.ENTER);
-Thread.sleep(5000);
-List allProductsName = driver.findElementsByClassName("android.widget.TextView");
-assert(allProductsName.size() > 0);
-((JavascriptExecutor) driver).executeScript("lambda-status=passed");
-}catch (Exception e){
-((JavascriptExecutor) driver).executeScript("lambda-status=failed");
-e.printStackTrace();
-}catch (AssertionError a){
-((JavascriptExecutor) driver).executeScript("lambda-status=failed");
-a.printStackTrace();
+        // Set the listeners for the buttons.
+        findViewById(R.id.changeTextBt).setOnClickListener(this);
+        findViewById(R.id.activityChangeTextBtn).setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.textToBeChanged);
+        mEditText = (EditText) findViewById(R.id.editTextUserInput);
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Get the text from the EditText view.
+        final String text = mEditText.getText().toString();
+
+        final int changeTextBtId = R.id.changeTextBt;
+        final int activityChangeTextBtnId = R.id.activityChangeTextBtn;
+
+        if (view.getId() == changeTextBtId) {
+          // First button's interaction: set a text in a text view.
+          mTextView.setText(text);
+        } else if (view.getId() == activityChangeTextBtnId) {
+          // Second button's interaction: start an activity and send a message to it.
+          Intent intent = com.example.android.testing.espresso.BasicSample.ShowTextActivity
+              .newStartIntent(this, text);
+          startActivity(intent);
+        }
+    }
 }
-// The driver.quit statement is required, otherwise the test continues to execute, leading
-to a timeout.
-driver.quit();
-}
-}
+
+
+
+
+// import java.net.URL;
+// import java.util.List;
+// import java.net.MalformedURLException;
+// import io.appium.java_client.MobileBy;
+// import io.appium.java_client.MobileElement;
+// import io.appium.java_client.android.AndroidDriver;
+// import io.appium.java_client.android.AndroidElement;
+// import org.openqa.selenium.JavascriptExecutor;
+// import org.openqa.selenium.Keys;
+// import org.openqa.selenium.remote.RemoteWebDriver;
+// import org.openqa.selenium.support.ui.ExpectedConditions;
+// import org.openqa.selenium.support.ui.WebDriverWait;
+// import org.openqa.selenium.remote.DesiredCapabilities;
+// public class AndroidApp {
+// public static String userName = "nihalnevercode";
+// public static String accessKey = "tZAy17ivs9JlLPDqjB3n3sZxZq3K1SzPmaGwOFNddEG9kdYeJt";
+// public static void main(String args[]) throws MalformedURLException, InterruptedException {
+// DesiredCapabilities capabilities = new DesiredCapabilities();
+// capabilities.setCapability("platformName", "Android");
+// capabilities.setCapability("deviceName", "Google Pixel 3");
+// capabilities.setCapability("isRealMobile", true);
+// capabilities.setCapability("platformVersion","10");
+// capabilities.setCapability("app","lt://APP100201711643634891400553");
+// capabilities.setCapability("deviceOrientation", "PORTRAIT");
+// capabilities.setCapability("console",true);
+// capabilities.setCapability("network",true);
+// capabilities.setCapability("visual",true);
+// RemoteWebDriver driver = new RemoteWebDriver(new
+// URL("https://"+userName+":"+accessKey+"@beta-hub.lambdatest.com/wd/hub"), capabilities);
+// try {
+// AndroidElement searchElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+// ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Search
+// Wikipedia")));
+// searchElement.click();
+// AndroidElement insertTextElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+// ExpectedConditions.elementToBeClickable(MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
+// insertTextElement.sendKeys("LambdaTest"+ Keys.ENTER);
+// Thread.sleep(5000);
+// List allProductsName = driver.findElementsByClassName("android.widget.TextView");
+// assert(allProductsName.size() > 0);
+// ((JavascriptExecutor) driver).executeScript("lambda-status=passed");
+// }catch (Exception e){
+// ((JavascriptExecutor) driver).executeScript("lambda-status=failed");
+// e.printStackTrace();
+// }catch (AssertionError a){
+// ((JavascriptExecutor) driver).executeScript("lambda-status=failed");
+// a.printStackTrace();
+// }
+// // The driver.quit statement is required, otherwise the test continues to execute, leading
+// to a timeout.
+// driver.quit();
+// }
+// }
